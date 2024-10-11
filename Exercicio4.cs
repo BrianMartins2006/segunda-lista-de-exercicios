6cfed3b870769397b8fa2554e77f022bb69f699d
BrianMartins2006 <brianmartins997232779@gmail.com>
2024-10-11 09:42:32 -0300

    Add files via upload

---
+++ Exercicio4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Arrays;
+
+namespace vetores
+{
+    internal class Exercicio4
+    {
+        static int impar(int[] vetor)
+        {
+            int Nimpar=0;
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                if (vetor[i] % 2 != 0)
+                    Nimpar++;
+            }
+            return Nimpar;
+           
+        }
+            static void Main()
+            {
+                int n,Nimpar;
+                Console.WriteLine("tamanho do vetor :");
+                n = int.Parse(Console.ReadLine());
+                int[] meuvetor = new int[n];
+                BibliotecaArray.geravetor(meuvetor);
+                Console.WriteLine("Vetor gerado: ");
+                BibliotecaArray.mostreVetor(meuvetor);
+                Console.WriteLine($" \n Foram digitados {impar(meuvetor)} numeros impares");
+                Console.ReadKey();
+
+            }
+        }
+    }
