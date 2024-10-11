ba8b41022734408b079bf72157e98a26e1b1fc4e
BrianMartins2006 <brianmartins997232779@gmail.com>
2024-10-11 08:31:35 -0300

    Add files via upload

---
+++ Exercicio3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Arrays;
+
+    internal class Exercicio3
+    {
+    static int menor(int[] vetor)
+    {
+        int menor = 99999;
+
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            if (vetor[i] < menor)
+            {
+                menor = vetor[i];
+            }
+        }
+        return menor;
+    }
+    static void Main()
+    {
+        int n;
+        Console.WriteLine("tamanho do vetor :");
+        n = int.Parse(Console.ReadLine());
+        int[] meuvetor = new int[n];
+        BibliotecaArray.geravetor(meuvetor);
+        Console.WriteLine("Vetor gerado: ");
+        BibliotecaArray.mostreVetor(meuvetor);
+        Console.WriteLine($" \n o menor numero é {menor(meuvetor)}");
+        Console.ReadKey();
+
+    }
+}
