4b497658ce00d1a5de03bec960de3d3adfa8db71
BrianMartins2006 <brianmartins997232779@gmail.com>
2024-10-22 15:25:15 -0300

    Add files via upload

---
+++ Exercicio8.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Segunda_lista_de_exercicio
+{
+    internal class Exercicio8
+    {
+        static void Main()
+        {
+            Console.WriteLine("Digite um texto para ser escrito ao contrário:");
+            string Texto = Console.ReadLine();
+            char[] vetor = Texto.ToCharArray();
+            int length = vetor.Length;
+            for (int i = length - 1; i >= 0; i--)
+            {
+                Console.Write(vetor[i]);
+            }
+            Console.WriteLine(); // Para terminar a linha
+        }
+    }
+}
